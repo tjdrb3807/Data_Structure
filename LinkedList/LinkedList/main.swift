4a897bb43b0f439082deb0f9e289f3f7a2810cfd
Jeon_Seong_gyu <junsung3807@naver.com>
2024-11-20 20:34:01 +0900

    🎉 Begin a project of binary search tree

--- LinkedList/LinkedList/main.swift
+++ LinkedList/LinkedList/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+struct LinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    var isEmpty: Bool { head == nil }
+    
+    mutating func push(_ data: T) {
+        head = Node(data: data, next: head)
+        if tail == nil { tail = head }
+    }
+}
 
+var list = LinkedList<Int>()
+list.push(3)
+list.push(2)
+list.push(1)
+
+print(list)
---
+++ Tree/BinarySearchTree/BinarySearchTree/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BinarySearchTree
+//
+//  Created by 전성규 on 11/20/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
